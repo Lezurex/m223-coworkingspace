bdac57dad8e763ea6723e1cb5557359f99e5b1ba
Lenny <lenny@familie-angst.ch>
2022-12-07 09:41:14 +0000

    chore: file formatting

--- src/test/java/com/lezurex/m223/coworkingspace/ApplicationUserResourceTest.java
+++ src/test/java/com/lezurex/m223/coworkingspace/ApplicationUserResourceTest.java
@@ -18,35 +18,35 @@ import io.restassured.http.ContentType;
 @TestSecurity(user = "hans@test.com", roles = "user")
 public class ApplicationUserResourceTest {
 
-    @Test
-    public void testIndexEndpoint() {
-        given().when().get().then().statusCode(200).body(startsWith("[")).and().body(endsWith("]"));
-    }
+  @Test
+  public void testIndexEndpoint() {
+    given().when().get().then().statusCode(200).body(startsWith("[")).and().body(endsWith("]"));
+  }
 
-    @Test
-    public void testPostEndpoint() {
-        var payload = new ApplicationUser("thanam.pangri@tbsana.ch", "Thanam", "Pangri",
-                "PangriFTW123", RoleEnum.MEMBER);
+  @Test
+  public void testPostEndpoint() {
+    var payload = new ApplicationUser("thanam.pangri@tbsana.ch", "Thanam", "Pangri", "PangriFTW123",
+        RoleEnum.MEMBER);
 
-        given().when().contentType(ContentType.JSON).body(payload).post().then().statusCode(200)
-                .body("email", is("thanam.pangri@tbsana.ch"));
-    }
+    given().when().contentType(ContentType.JSON).body(payload).post().then().statusCode(200)
+        .body("email", is("thanam.pangri@tbsana.ch"));
+  }
 
-    @Test
-    public void testPutEndpoint() {
-        var payload = new ApplicationUser("thanam.pangri@tbsana.ch", "Thanam", "Pangri",
-                "PangriFTW123", RoleEnum.MEMBER);
+  @Test
+  public void testPutEndpoint() {
+    var payload = new ApplicationUser("thanam.pangri@tbsana.ch", "Thanam", "Pangri", "PangriFTW123",
+        RoleEnum.MEMBER);
 
-        given().when().contentType(ContentType.JSON).body(payload).post();
-        payload.setPasswordHash("ThanamFTW123");
-        payload.setRole(RoleEnum.ADMIN);
-        given().when().contentType(ContentType.JSON).body(payload).put("/3").then().statusCode(200)
-                .body("passwordHash", is("ThanamFTW123"));
-    }
+    given().when().contentType(ContentType.JSON).body(payload).post();
+    payload.setPasswordHash("ThanamFTW123");
+    payload.setRole(RoleEnum.ADMIN);
+    given().when().contentType(ContentType.JSON).body(payload).put("/3").then().statusCode(200)
+        .body("passwordHash", is("ThanamFTW123"));
+  }
 
-    @Test
-    public void testDeleteEndpoint() {
-        given().when().delete("/2").then().statusCode(204);
-    }
+  @Test
+  public void testDeleteEndpoint() {
+    given().when().delete("/2").then().statusCode(204);
+  }
 
 }
