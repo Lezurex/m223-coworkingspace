920a1bd7a72ab2cd412adc42c2a5fda96cec922b
Lenny <lenny@familie-angst.ch>
2022-12-08 12:14:25 +0000

    doc: add session OpenAPI descriptions

--- src/main/java/com/lezurex/m223/coworkingspace/controller/SessionController.java
+++ src/main/java/com/lezurex/m223/coworkingspace/controller/SessionController.java
@@ -7,10 +7,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import com.lezurex.m223.coworkingspace.model.Credential;
 import com.lezurex.m223.coworkingspace.service.SessionService;
 
 @Path("/session")
+@Tag(name = "Session", description = "Allows a user to manage his session.")
 public class SessionController {
 
   @Inject
@@ -19,6 +22,8 @@ public class SessionController {
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
+  @Operation(summary = "Allows the user to login.",
+      description = "This endpoint allows the user to login. On successful login, a JWT (valid for 24 hours) will be returned in the Authorization header.")
   public Response login(Credential credential) {
     return sessionService.authenticate(credential);
   }
